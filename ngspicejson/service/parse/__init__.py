4ec1bb377fa356afb23b2f641314adbffe1aaf52
devkingsejong <devkingsejong@gmail.com>
2021-03-12 00:42:54 +0900

    __init__파일의 자동 변경된 부분을 삭제합니다.

--- ngspicejson/service/parse/__init__.py
+++ ngspicejson/service/parse/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['abstract_parse', 'parse_model_list.py', 'parse_initial_transient_solution.py', 'parse_print_tabular_contents.py']
+__all__ = ['abstract_parse', 'parse_model_list', 'parse_initial_transient_solution', 'parse_print_tabular_contents']
