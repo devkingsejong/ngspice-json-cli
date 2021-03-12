a2fed7666bd076038ecff271edf951e13a923e73
devkingsejong <devkingsejong@gmail.com>
2021-03-12 21:04:49 +0900

    NJC-18 # 특정 지점을 지정하여 출력한 정보를 파싱하는 기능을 구현합니다.

--- ngspicejson/service/parse/__init__.py
+++ ngspicejson/service/parse/__init__.py
@@ -1,2 +1,2 @@
 __all__ = ['abstract_parse', 'parse_model_list', 'parse_initial_transient_solution', 'parse_print_tabular_contents',
-           'parse_ngspice_version']
+           'parse_ngspice_version', 'parse_specific_print']
